f1e259915865372e0d866cf244f3f1583ae35657
izdinmor <izdinmor@gmail.com>
2021-12-06 13:09:09 +0100

    main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //load next level
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //quit the application
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
