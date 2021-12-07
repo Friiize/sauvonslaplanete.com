0043278687ff5d344faea33d7988c0b29135454c
Raffaele Leruste <raffaele.leruste@gmail.com>
2021-12-07 12:18:57 +0100

    EventHandler.cs added

---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class EventHandler : MonoBehaviour
+    {
+        [HideInInspector]
+        public static EventHandler Instance;        
+        public float increase;
+        public int eventId = 0;
+
+
+        private void Awake()
+        {
+            if (Instance) throw new NotImplementedException("PLus d'une fois le script dans la scène !");
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            InvokeRepeating("NextEvent", 2f, increase);
+        }
+
+        private void NextEvent()
+        {
+            eventId = Random.Range(0, 2);
+        }
+    }
+}
