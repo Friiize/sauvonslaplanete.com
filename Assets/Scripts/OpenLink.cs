7e2c9ee2d45652a4ba3c02ffd8e08552d96431b6
Raffaele Leruste <raffaele.leruste@gmail.com>
2021-12-10 18:50:21 +0100

    likujkhgl

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenIPCCLink()
+    {
+        Application.OpenURL("https://www.ipcc.ch/");
+    }
+}
